e109c515c56a942a995d8647b171f1c0053beb0c
DaoPhang <dtphang05@gmail.com>
2025-10-15 19:03:25 +0800

    Update

---
+++ Week1/App1.java
@@ -0,0 +1,35 @@
+public class App1 {
+    public static void main(String[] args){
+        Person person1 = new Person();
+        person1.name = "Bruh";
+        person1.height= 1.8;
+
+        Person person2 = new Person();
+        person2.name = "Sara";
+        person2.height =2.0;
+
+        System.out.println(person1.name + " height's is " + person1.height);
+        System.out.print(person2.name + person2.sleeping());
+    }
+}
+
+class Person{
+        //1.data
+        String name;
+        protected double height;
+
+        //2. methods
+        String sleeping(){
+            return "is sleeping";
+        }
+
+        void eat(){
+            System.out.println("is eating something");
+        }
+    }
+
+class Student{
+    void eat(){
+        System.out.println("Student on a diet...");
+    }
+}
