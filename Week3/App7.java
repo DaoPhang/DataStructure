69d32f41f206ee115abf97844777cbb2d7bbe3d7
DaoPhang <dtphang05@gmail.com>
2025-10-29 20:33:51 +0800

    Week3

--- Week3/App7.java
+++ Week3/App7.java
@@ -1,4 +1,5 @@
 package DS.Week3;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,6 +15,27 @@ public class App7 {
 
         String fruit = (String)a1.get(0);
         System.out.println(fruit);
+
+        for(int i = 0; i <3; i++){
+            fruit = (String)a1.get(i);
+            System.out.println(fruit);
+        }
+
+        ArrayList<String> l1 = new ArrayList<String>();
+            l1.add("cat");
+            l1.add("mouse");
+            l1.add("bat");
+            System.out.println(l1);
+            for(String numbers : l1){
+                System.out.println(numbers);
+            }
+            
+            ArrayList<Integer>l2 = new ArrayList<Integer>();
+            l2.add(1000);
+            l2.add(10000000);
+            System.out.println(l2);
+
+            ArrayList<Double>l3 = new ArrayList<Double>();
     }
 
 }
